779059c6ef3f8ef44689f40591c33f3c32195b08
Erno <54817547+pajerr@users.noreply.github.com>
2022-09-08 22:09:31 +0300

    added healtcheck test

--- draft/main.go
+++ draft/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"io"
 	"net/http"
 )
 
@@ -37,6 +38,15 @@ func supplementsHandler(w http.ResponseWriter, r *http.Request) {
 		//write the JSON to the response to the client
 		w.Write(supplementsJSON)
 	}
+}
+
+func HealthCheckHandler(w http.ResponseWriter, r *http.Request) {
+	// A very simple health check.
+	w.WriteHeader(http.StatusOK)
+	w.Header().Set("Content-Type", "application/json")
+	// In the future we could report back on the status of our DB, or our cache
+	// (e.g. Redis) by performing a simple PING, and include them in the response.
+	io.WriteString(w, `{"alive": true}`)
 }
 
 func main() {
