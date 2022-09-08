a61fdc5a2a47b2af1457ebd432ea9e08c1ef79a1
Erno <54817547+pajerr@users.noreply.github.com>
2022-09-08 21:30:48 +0300

    added test file

--- draft/main.go
+++ draft/main.go
@@ -1,25 +1,58 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
 
 //type Supplement interface {
 //Details() string
 //}
 
+var supplements []Supplement
+
 type Supplement struct {
-	Name   string
-	Dosage int
-	Unit   string
+	Name   string `json:"Name"`
+	Dosage int    `json:"Dosage"`
+	Unit   string `json:"Unit"`
 }
 
-//Create empty map of Supplements and append supplements to it
+//function to add supplement to slice, will be replaced with DB
+func addSupplement(supplement Supplement) {
+	supplements = append(supplements, supplement)
+}
+
+func supplementsHandler(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	//if client sends GET request
+	case http.MethodGet:
+		supplementsJSON, err := json.Marshal(supplements)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		w.WriteHeader(http.StatusOK)
+		w.Header().Set("Content-Type", "application/json")
+		//write the JSON to the response to the client
+		w.Write(supplementsJSON)
+	}
+}
 
 func main() {
-	var supplements []Supplement
+	//Create empty map of Supplements and append supplements to it
+	//will be replaced by DB
+	//var supplements []Supplement
 	supplements = append(supplements, Supplement{"Vitamin C", 500, "mg"})
 	supplements = append(supplements, Supplement{"Vitamin D", 1000, "IU"})
 
 	for range supplements {
 		fmt.Println(supplements)
 	}
+
+	http.HandleFunc("/supplements", supplementsHandler)
+	http.ListenAndServe(":8088", nil)
+	//test to implement GET method
+	//curl localhost:8088/supplements
+	//[{"Name":"Vitamin C","Dosage":500,"Unit":"mg"},{"Name":"Vitamin D","Dosage":1000,"Unit":"IU"}]
 }
