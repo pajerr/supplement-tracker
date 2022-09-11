f7c343268a56231fbe0530fef02960d84d50fd74
Erno <54817547+pajerr@users.noreply.github.com>
2022-09-11 16:49:04 +0300

    seperated main.go and server.go

--- main.go
+++ main.go
@@ -1,29 +1,8 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 )
-
-//var supplementList []Supplement
-
-type Supplement struct {
-	Name   string `json:"Name"`
-	Dosage int    `json:"Dosage"`
-	Unit   string `json:"Unit"`
-}
-
-func supplementsHandler(w http.ResponseWriter, r *http.Request) {
-	//hardcoded test data
-	testVitaminC := Supplement{Name: "Vitamin C", Dosage: 500, Unit: "mg"}
-	switch r.Method {
-	//if client sends GET request
-	case http.MethodGet:
-		w.WriteHeader(http.StatusOK)
-		//write dosage of Vitamin C to the response
-		fmt.Fprintf(w, "%v", testVitaminC.Dosage)
-	}
-}
 
 func main() {
 	http.HandleFunc("/supplements", supplementsHandler)
---
+++ server.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+type Supplement struct {
+	Name   string `json:"Name"`
+	Dosage int    `json:"Dosage"`
+	Unit   string `json:"Unit"`
+}
+
+func supplementsHandler(w http.ResponseWriter, r *http.Request) {
+	//hardcoded test data
+	testVitaminC := Supplement{Name: "Vitamin C", Dosage: 500, Unit: "mg"}
+	switch r.Method {
+	//if client sends GET request
+	case http.MethodGet:
+		w.WriteHeader(http.StatusOK)
+		//write dosage of Vitamin C to the response
+		fmt.Fprintf(w, "%v", testVitaminC.Dosage)
+	}
+}
