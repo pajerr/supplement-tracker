d63c63792d357ed0b9a0d1c19710b3e59f1fde4a
Erno <54817547+pajerr@users.noreply.github.com>
2022-09-11 16:39:04 +0300

    simple test

--- main.go
+++ main.go
@@ -1,17 +1,11 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
-	"io"
 	"net/http"
 )
 
-//type Supplement interface {
-//Details() string
-//}
-
-var supplements []Supplement
+//var supplementList []Supplement
 
 type Supplement struct {
 	Name   string `json:"Name"`
@@ -19,51 +13,19 @@ type Supplement struct {
 	Unit   string `json:"Unit"`
 }
 
-//function to add supplement to slice, will be replaced with DB
-func addSupplement(supplement Supplement) {
-	supplements = append(supplements, supplement)
-}
-
 func supplementsHandler(w http.ResponseWriter, r *http.Request) {
+	//hardcoded test data
+	testVitaminC := Supplement{Name: "Vitamin C", Dosage: 500, Unit: "mg"}
 	switch r.Method {
 	//if client sends GET request
 	case http.MethodGet:
-		supplementsJSON, err := json.Marshal(supplements)
-		if err != nil {
-			w.WriteHeader(http.StatusInternalServerError)
-			return
-		}
 		w.WriteHeader(http.StatusOK)
-		w.Header().Set("Content-Type", "application/json")
-		//write the JSON to the response to the client
-		//w.Write(supplementsJSON)
-		fmt.Fprint(w, string(supplementsJSON))
+		//write dosage of Vitamin C to the response
+		fmt.Fprintf(w, "%v", testVitaminC.Dosage)
 	}
-}
-
-func HealthCheckHandler(w http.ResponseWriter, r *http.Request) {
-	// A very simple health check.
-	w.WriteHeader(http.StatusOK)
-	w.Header().Set("Content-Type", "application/json")
-	// In the future we could report back on the status of our DB, or our cache
-	// (e.g. Redis) by performing a simple PING, and include them in the response.
-	io.WriteString(w, `{"alive": true}`)
 }
 
 func main() {
-	//Create empty map of Supplements and append supplements to it
-	//will be replaced by DB
-	//var supplements []Supplement
-	supplements = append(supplements, Supplement{"Vitamin C", 500, "mg"})
-	supplements = append(supplements, Supplement{"Vitamin D", 1000, "IU"})
-
-	for range supplements {
-		fmt.Println(supplements)
-	}
-
 	http.HandleFunc("/supplements", supplementsHandler)
 	http.ListenAndServe(":8088", nil)
-	//test to implement GET method
-	//curl localhost:8088/supplements
-	//[{"Name":"Vitamin C","Dosage":500,"Unit":"mg"},{"Name":"Vitamin D","Dosage":1000,"Unit":"IU"}]
 }
--- main_test.go
+++ main_test.go
@@ -1,63 +1,23 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 )
 
-func TestSupplementHandler(t *testing.T) {
-	// Create a request to pass to our handler. We don't have any query parameters for now, so we'll
-	// pass 'nil' as the third parameter.
-	req, err := http.NewRequest("GET", "/supplements", nil)
-	if err != nil {
-		t.Fatal(err)
-	}
-	// We create a ResponseRecorder (which satisfies http.ResponseWriter) to record the response.
-	res := httptest.NewRecorder()
-	handler := http.HandlerFunc(supplementsHandler)
-	// Our handlers satisfy http.Handler, so we can call their ServeHTTP method
-	// directly and pass in our Request and ResponseRecorder.
-	handler.ServeHTTP(res, req)
+func TestVitaminCDosage(t *testing.T) {
+	request, _ := http.NewRequest(http.MethodGet, "/vitaminc", nil)
+	response := httptest.NewRecorder()
 
-	// debug printting reponse body
-	fmt.Printf("**** request variable content: ****%v\n", res.Body.String())
-	// Check the status code is what we expect.
-	if status := res.Code; status != http.StatusOK {
-		t.Errorf("handler returned wrong status code: got %v want %v",
-			status, http.StatusOK)
-	}
-	// Check the response body is what we expect.
-	expected := `{"Name":"Vitamin C","Dosage":500,"Unit":"mg"},{"Name":"Vitamin D","Dosage":1000,"Unit":"IU"}`
-	if res.Body.String() != expected {
-		t.Errorf("handler returned unexpected body: got %v want %v",
-			res.Body.String(), expected)
-	}
-}
+	supplementsHandler(response, request)
 
-func TestHealthCheckHandler(t *testing.T) {
-	// Create a request to pass to our handler. We don't have any query parameters for now, so we'll
-	// pass 'nil' as the third parameter.
-	req, err := http.NewRequest("GET", "/health-check", nil)
-	if err != nil {
-		t.Fatal(err)
-	}
-	// We create a ResponseRecorder (which satisfies http.ResponseWriter) to record the response.
-	rr := httptest.NewRecorder()
-	handler := http.HandlerFunc(HealthCheckHandler)
-	// Our handlers satisfy http.Handler, so we can call their ServeHTTP method
-	// directly and pass in our Request and ResponseRecorder.
-	handler.ServeHTTP(rr, req)
-	// Check the status code is what we expect.
-	if status := rr.Code; status != http.StatusOK {
-		t.Errorf("handler returned wrong status code: got %v want %v",
-			status, http.StatusOK)
-	}
-	// Check the response body is what we expect.
-	expected := `{"alive": true}`
-	if rr.Body.String() != expected {
-		t.Errorf("handler returned unexpected body: got %v want %v",
-			rr.Body.String(), expected)
-	}
+	t.Run("Return Vitamin C dosage", func(t *testing.T) {
+		got := response.Body.String()
+		want := "500"
+
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	})
 }
