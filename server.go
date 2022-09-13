b8853b3aaf4b9c4fc180765f9ab2e7c1d8acdac0
Erno <54817547+pajerr@users.noreply.github.com>
2022-09-13 20:01:39 +0300

    added test file for now to gitrepo

---
+++ server.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"strings"
+)
+
+type SupplementDataStore interface {
+	GetSupplementDosage(name string) int
+}
+
+//allows us to use the SupplementDataStore interface in the handler
+//for example to store.GetSupplelementDosage to get supplements dosage
+type supplementsHandler struct {
+	store SupplementDataStore
+}
+
+//Refactored to use SupplementDataStore interface
+func (s *supplementsHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+
+	switch r.Method {
+	case http.MethodPost:
+		s.processSetDosage(w)
+	case http.MethodGet:
+		s.showDosage(w, r)
+	}
+
+}
+
+func (s *supplementsHandler) showDosage(w http.ResponseWriter, r *http.Request) {
+	//r.URL.Path returns the path of the request which we can then use strings.TrimPrefix to trim away /supplements/
+	supplement := strings.TrimPrefix(r.URL.Path, "/supplements/")
+	dosage := s.store.GetSupplementDosage(supplement)
+
+	//if supplement not in StubSupplementDataStore return 404
+	if supplement != "magnesium" && supplement != "vitamin-c" {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+
+	fmt.Fprint(w, dosage)
+
+}
+
+func (s *supplementsHandler) processSetDosage(w http.ResponseWriter) {
+	//return 200 status code if request is POST method to pass test
+	w.WriteHeader(http.StatusAccepted)
+}
---
+++ server_test.go
@@ -0,0 +1,104 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+//A map is a quick and easy way of making a stub key/value store for our tests
+type StubSupplementDataStore struct {
+	dosages map[string]int
+}
+
+func (stub *StubSupplementDataStore) GetSupplementDosage(name string) int {
+	dosage := stub.dosages[name]
+	return dosage
+}
+
+func TestSupplementDosages(t *testing.T) {
+	//create stub data store
+	store := StubSupplementDataStore{
+		map[string]int{
+			"vitamin-c": 500,
+			"magnesium": 400,
+		},
+	}
+
+	//create a new instance of our supplementsHandler and then call its method ServeHTTP
+	//send in the stub data store as the argument to the supplementsHandler/server
+	server := &supplementsHandler{&store}
+
+	t.Run("Return Vitamin C dosage", func(t *testing.T) {
+		//Use helper function to create a new GET request for Vitamin C
+		request := newGetSupplementDosage("vitamin-c")
+		response := httptest.NewRecorder()
+
+		//we pass in the response and request to the ServeHTTP method from our supplementsHandler
+		server.ServeHTTP(response, request)
+
+		assertStatus(t, response.Code, http.StatusOK)
+		assertResponseBody(t, response.Body.String(), "500")
+	})
+
+	t.Run("Return Magnesium dosage", func(t *testing.T) {
+		//Use helper function to create a new GET request for Vitamin C
+		request := newGetSupplementDosage("magnesium")
+		response := httptest.NewRecorder()
+
+		server.ServeHTTP(response, request)
+
+		assertStatus(t, response.Code, http.StatusOK)
+		assertResponseBody(t, response.Body.String(), "400")
+	})
+
+	t.Run("Return 404 on missing supplement", func(t *testing.T) {
+		request := newGetSupplementDosage("iron")
+		response := httptest.NewRecorder()
+
+		server.ServeHTTP(response, request)
+
+		assertStatus(t, response.Code, http.StatusNotFound)
+	})
+}
+
+//Testing that POST reponse gets accepted
+func TestStoreDosage(t *testing.T) {
+	store := StubSupplementDataStore{
+		map[string]int{},
+	}
+
+	server := &supplementsHandler{&store}
+
+	t.Run("it returns accepted on POST", func(t *testing.T) {
+		request, _ := http.NewRequest(http.MethodPost, "/supplements/magnesium", nil)
+		response := httptest.NewRecorder()
+
+		server.ServeHTTP(response, request)
+
+		assertStatus(t, response.Code, http.StatusAccepted)
+	})
+}
+
+//Helper functon to create a new GET request for a supplement
+func newGetSupplementDosage(supplementName string) *http.Request {
+	req, _ := http.NewRequest(http.MethodGet, fmt.Sprintf("/supplements/%s", supplementName), nil)
+	return req
+}
+
+//Helper function to check the response body
+func assertResponseBody(t testing.TB, got, want string) {
+	t.Helper()
+	if got != want {
+		t.Errorf("response body is wrong, got %q want %q", got, want)
+	}
+}
+
+//Helper function to check the response status
+func assertStatus(t testing.TB, got, want int) {
+	t.Helper()
+	if got != want {
+		t.Errorf("did not get correct status, got %d, want %d", got, want)
+	}
+}
